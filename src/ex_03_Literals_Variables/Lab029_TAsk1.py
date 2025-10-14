4fdf8ba5cf78287cd946e744ae8c6dfa10789db6
YogiGithubTest <yogendrarajput5794@gmail.com>
2025-10-14 07:05:14 +0530

    commit all

---
+++ src/ex_03_Literals_Variables/Lab029_TAsk1.py
@@ -0,0 +1,7 @@
+num1 =float(input("Enter a number: "))
+num2 =float(input("Enter another number: "))
+num3 =float(input("Enter a third number: "))
+sub_result = num1 - num2-num3
+print(sub_result)
+sum_result = num1 + num2 + num3
+print(sum_result)
